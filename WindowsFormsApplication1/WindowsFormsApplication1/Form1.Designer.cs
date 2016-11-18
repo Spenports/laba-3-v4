bcf474dc44633c53a8bb915558f90d9027b558cc
sPenchi <mr.pronenko@bk.ru>
2016-11-18 13:12:25 +0300

    " 2 комит на девлопе добавил текст боксы"

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,20 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.T2_textBox2 = new System.Windows.Forms.TextBox();
+            this.T2_textBox1 = new System.Windows.Forms.TextBox();
+            this.T2_textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // T2_textBox2
+            // 
+            this.T2_textBox2.Location = new System.Drawing.Point(236, 65);
+            this.T2_textBox2.Name = "T2_textBox2";
+            this.T2_textBox2.Size = new System.Drawing.Size(139, 20);
+            this.T2_textBox2.TabIndex = 11;
+            // 
+            // T2_textBox1
+            // 
+            this.T2_textBox1.Location = new System.Drawing.Point(23, 65);
+            this.T2_textBox1.Name = "T2_textBox1";
+            this.T2_textBox1.Size = new System.Drawing.Size(141, 20);
+            this.T2_textBox1.TabIndex = 10;
+            // 
+            // T2_textBox3
+            // 
+            this.T2_textBox3.Location = new System.Drawing.Point(70, 361);
+            this.T2_textBox3.Name = "T2_textBox3";
+            this.T2_textBox3.Size = new System.Drawing.Size(262, 20);
+            this.T2_textBox3.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(406, 421);
+            this.Controls.Add(this.T2_textBox3);
+            this.Controls.Add(this.T2_textBox2);
+            this.Controls.Add(this.T2_textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox T2_textBox2;
+        private System.Windows.Forms.TextBox T2_textBox1;
+        private System.Windows.Forms.TextBox T2_textBox3;
     }
 }
 
